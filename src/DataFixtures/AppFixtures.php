4298ae480deaa3dc11021dbc0c93db5ea629005b
AnastasiyaHahanova <anastasiya.hahanova@mail.ru>
2020-01-27 18:18:58 +0300

    my commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Book;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(\Doctrine\Persistence\ObjectManager $manager)
+    {
+        $book = new Book();
+        $book->setYear(1964);
+        $book->setName("Праздник, который всегда с тобой");
+        $book->setAuthor("Э.Хэмингуэй");
+        $manager->persist($book);
+
+        $book = new Book();
+        $book->setYear(1868);
+        $book->setName("Идиот");
+        $book->setAuthor("Ф.М. Достоевский");
+        $manager->persist($book);
+
+        $book = new Book();
+        $book->setYear(1878);
+        $book->setName("Анна Каренина");
+        $book->setAuthor("Л.Н. Толстой");
+        $manager->persist($book);
+
+        $book = new Book();
+        $book->setYear(1973);
+        $book->setName("В краю лесов");
+        $book->setAuthor("Т.Харди");
+        $manager->persist($book);
+
+        $book = new Book();
+        $book->setYear(1847);
+        $book->setName("Джейн Эйр");
+        $book->setAuthor("Ш.Бронте");
+        $manager->persist($book);
+
+
+        $manager->flush();
+    }
+}
