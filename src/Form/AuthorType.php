610ec9718e47e282b5e59453ff5ada1a3612f6a3
AnastasiyaHahanova <anastasiya.hahanova@mail.ru>
2020-06-05 00:34:33 +0300

    edit Entity

--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -2,22 +2,23 @@
 
 namespace App\Form;
 
-use App\Entity\Book;
+use App\Entity\Author;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class BookType extends AbstractType
+class AuthorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('year', NumberType::class)
-            ->add('author', TextType::class)
+            ->add('firstName', TextType::class)
+            ->add('secondName',TextType::class)
+            ->add('patronymic', TextType::class)
+            ->add('birthDate', \DateTime::class)
+            ->add('country', TextType::class)
             ->add('submit', SubmitType::class)
         ;
     }
@@ -25,7 +26,7 @@ class BookType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Book::class,
+            'data_class' => Author::class,
         ]);
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('firstName', TextType::class)
+            ->add('secondName', TextType::class)
+            ->add('patronymic', TextType::class)
+            ->add('email', TextType::class)
+            ->add('birthDate', \DateTime::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
